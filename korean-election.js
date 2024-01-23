139359c2fd7f77cdce5a3ab3f754a57aeed2ed37
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-01-23 20:09:37 +0900

    Add files via upload

---
+++ korean-election.js
@@ -0,0 +1,22 @@
+class Election extends React.Component {
+	constructor(props){
+		super(props);
+		this.state= { votes: '넉넉한 사전투표 기간?'}
+	}
+	componentDidMount() {
+		setTimeout( () => {
+			this.setState({ votes: ' 사전투표 즉시 폐기!'});
+		} , 2000 );
+	}
+	render() {
+		return (
+			<p>
+				소중한 투표 권리행사만큼 중요한 것은<br />
+				<b className= 'red'>{this.state.votes}</b>
+			</p>
+		);
+	}
+}
+
+const yellow = ReactDOM.createRoot( document.getElementById('root') );
+yellow.render( <Election /> );
