f1f9e07c78aad683cc82b0b7f56cb7b6cd44f877
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-01-23 19:38:18 +0900

    Add files via upload

---
+++ BarleyTree.js
@@ -0,0 +1,32 @@
+class Barley extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			mun: '후문 뒤  ',
+			communism: '우물 곁에 ',
+			eradicate: '서 있는 보리수'
+		};
+	}
+	change =()=> {
+     	 this.setState({mun: '성문 앞  '});
+	}
+	render(){
+		return (
+			<section>
+				<h1 className='orange'>빌헬름 뮐러_ 보리수</h1>
+				<p>
+					<b className='aqua'>{ this.state.mun }</b>
+					{ this.state.communism }
+					{ this.state.eradicate }<br />
+					나는 그 그늘 아래 단 꿈을 보았네<br />
+					가지에 희망의 말 새기어 놓고서<br />
+					기쁘나 슬플 때나 찾아온 나무 밑
+				</p>
+ 				<button type='button' onClick={this.change}>텍스트 변경하기</button>
+			</section>
+		);
+	}
+}
+
+const myApp = ReactDOM.createRoot( document.getElementById('root') );
+myApp.render( <Barley /> );
