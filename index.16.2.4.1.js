42cd0a1d656a3d673f616d5589963daf3cb99f35
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-01-24 17:47:55 +0900

    Add files via upload

---
+++ index.16.2.4.1.js
@@ -0,0 +1,17 @@
+function Barley () {
+	const [ txt, setTextarea ]= useState ('보리수 가사를 입력하세요.');
+		const writeTxt= ( event ) => {
+		setTextarea ( event.target.value );
+	}
+		return (
+		<section>
+			<h1>가곡_ 보리수</h1>
+			<form>
+				<textarea value= { txt } onChange= { writeTxt } />
+			</form>
+		</section>
+	)
+}
+
+const textBox = ReactDOM.createRoot ( document.getElementById ('root') );
+textBox.render ( <Barley /> );
