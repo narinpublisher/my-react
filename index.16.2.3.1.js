1f5d3915d4cc08e9b29bd8d7365cb195e81f0d86
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-01-24 17:40:26 +0900

    Add files via upload

---
+++ index.16.2.3.1.js
@@ -0,0 +1,29 @@
+function Tree () {
+	const [ inputs, setInputs ]= React.useState( { } );
+		const submitGoGo= ( event ) => {
+		const name=  event.target.name;
+		const value=  event.target.value;
+		setInputs ( values => ( { ...values, [name]: value } ) );
+	}
+		const submitGo= ( ev ) => 	{
+		ev.preventDefault ();
+		console.log ( inputs );
+	}
+		return (
+ 		<section>
+			<h1>가곡_ 비목</h1>
+			<form onSubmit= { submitGo }>
+			   	<input type= 'text' name= 'txt1'  value= { inputs.txt1 || ''} 
+					onChange= { submitGoGo } />
+				<br />깊은 계곡 양지 녘에<br />
+				<input type= 'text' name= 'txt2'  value= { inputs.txt2 || ''} 
+					onChange= { submitGoGo } />
+				<br />이름 모를 비목이여<br />
+				<button>전송하기</button>
+			</form>
+		</section>
+	)
+}
+
+const textBox = ReactDOM.createRoot ( document.getElementById ('root') );
+textBox.render ( <Tree /> );
