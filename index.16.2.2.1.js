99b01349a0059d2108e5b790146485ceff747a78
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-01-24 16:34:01 +0900

    Add files via upload

---
+++ index.16.2.2.1.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './myStyle.css';
+
+function Sunset () {
+	const [name, setName]= useState ('');
+	const submitGo= ( event ) => {
+			event.preventDefault ();
+			alert ( ` 입력하신 제목은 ${ name } 입니다. ` );
+	}
+	return (
+		<section>
+			<strong>
+				바람이 머물다 간 들판에<br />
+				모락 모락 피어나는 저녁 연기
+			</strong>
+			<form onSubmit= { submitGo }>
+					<br />
+					<label>
+						이 동요의 제목은? &nbsp;
+						<input type='text' value= { name }
+						  onChange= { ( e ) => setName( e.target.value )}  
+						/>
+					</label>
+			</form>
+		</section>
+	)
+}
+
+const textBox = ReactDOM.createRoot ( document.getElementById ('root') );
+textBox.render ( <Sunset /> );
