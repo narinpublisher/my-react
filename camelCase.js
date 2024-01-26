11891fa60dce36fd0750b6fd095e7ff1c242a1f1
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-01-26 11:50:06 +0900

    Add files via upload

---
+++ camelCase.js
@@ -0,0 +1,13 @@
+const CamelCase  = () => {
+	return (
+		<>
+			<h1 style= {{color: 'red' , backgroundColor: 'yellow' , padding: '10px 20px' , 
+			 borderRadius: '20px'}}>   인라인 스타일   </h1>
+			<p>인라인 CSS는 Javascript 객체로 작성되기 때문에 카멜케이스 구문으로 작성해야 합
+			니다.</p>
+		</>
+	);	
+}
+
+const addStyle = ReactDOM.createRoot ( document.getElementById ( 'camel' ) );
+addStyle.render ( <CamelCase /> );
