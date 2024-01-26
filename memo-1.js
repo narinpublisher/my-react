52a095dd885979bcee58c6a9117ea7eb23697b97
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-01-26 10:39:41 +0900

    Add files via upload

---
+++ memo-1.js
@@ -0,0 +1,35 @@
+const count = ( { total } ) => {
+		console.log ( '갱신 될 때만 렌더링됩니다.' );
+
+		return (
+			<section>
+				<h2>카운트</h2>
+				{ 
+					total.map ( ( txt , idx ) => {
+						return <num key = { idx }>  { txt }  </num> ;
+				} ) }
+			</section>
+		);
+};
+
+const MyApp = () => {
+	const [ number , add ] = useState ( 0 );
+	const [ increase , setTo ] = useState ( [ ] );
+		const increment = () => {
+			add ( ( i ) =>   i + 1 );
+		};
+		return (
+		<>
+			<Timer total = { increase } />
+			<hr />
+			<>
+				Count : { number } &nbsp;
+				<button onClick = { increment }>   +   </button>
+			</><br />
+			<b><small>[+]버튼을 클릭할 때마다 카운트 숫자는 증가합니다.</small></b>
+		</>
+	);
+}
+
+const clock = ReactDOM.createRoot ( document.getElementById ( 'memo' ) );
+clock.render ( <MyApp /> );
