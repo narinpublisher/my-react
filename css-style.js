d2b264dd837eb0a52d4e17c9fbb34d844a4e8360
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-01-26 12:04:22 +0900

    Add files via upload

---
+++ css-style.js
@@ -0,0 +1,11 @@
+const ExternalCss  = () => {	
+	return (
+		<section>
+			<h1>외부 파일로 적용되는 CSS 스타일시트</h1>
+			<p>CSS 스타일을 별도의 파일에 작성하고 파일 확장자가 파일명.css인 파일로 저장한 후 애플리케이션에서 불러와서 디자인 스타일이 적용되게 할 수 있습니다.</p>
+		</section>
+	);	
+}
+
+const addStyle = ReactDOM.createRoot ( document.getElementById ( 'style' ) );
+addStyle.render ( <ExternalCss /> );
