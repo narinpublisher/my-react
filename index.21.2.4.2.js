e53394deb9699ac0bd148ab63a9a8bad3a25fbff
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-02-17 16:13:11 +0900

    Add files via upload

--- index.21.2.4.2.js
+++ index.21.2.4.2.js
@@ -1,13 +1,17 @@
 function MyHair() {
-	const [hair, change] = React.useState('옐로우골드');
+	const [ hair, change ] = React.useState('옐로우골드');
 	
-	return (
-		<section style={{border:'2px dashed gold', padding: '10pt', textAlign: 'center'}}>
-			<h1>나의 헤어는 {hair}색 입니다.</h1>
-			<button onClick= { () =>  change("레드와인") }>색상 변경하기</button>
+	const color = ( i , j )  => {	
+		change( i );	
+		document.querySelector('section').style.border= " 2px dashed  " + j ;
+	}	
+ 	return (
+		<section style={{border: ' 2px dashed gold ', padding: '10pt', textAlign: 'center'}}>
+			<h1>나의 헤어는 { hair }색 입니다.</h1>
+			<button onClick= { () =>  color( '레드와인', 'red' ) }>색상 변경하기</button>
 		</section>
 	);
 }
 
 const myApp = ReactDOM.createRoot ( document.getElementById ( 'root' ));
-myApp.render ( <MyHair /> );
+myApp.render ( <MyHair /> );    
