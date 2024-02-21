858ce4095ab9e8fdb3a1a4e515705ee6888392c6
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2024-02-21 16:42:07 +0900

    Add files via upload

---
+++ 10.6.1.js
@@ -0,0 +1,16 @@
+const Lists = props => {
+  return <ul>{ props.children }</ul>
+}
+
+const Fruits = () => (
+	<Lists>
+		<li>딸기</li>
+		<li>파인애플</li>
+		<li>오렌지</li>
+	</Lists>
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById( 'root' ));
+root.render( <Fruits /> );
+
