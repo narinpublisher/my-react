7541c1016ba9854eb25eb33e2764f69d3214fc3b
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2025-08-07 18:32:10 +0900

    Update index.11.6.1.js

--- index.11.6.1.js
+++ index.11.6.1.js
@@ -1,21 +1,20 @@
-class Fruits  extends React.Component {
-   render(){
-      return <p>고개숙인 논밭의 열매</p>;
-   }
-}
+// 하위 컴포넌트
+const Fruits = () => {
+  return <p>고개숙인 논밭의 열매</p>;
+};
 
-class Yellow extends React.Component {
-   render(){
-      return (
-         <section>
-            <p>허수아비 팔 벌려 웃음짓고</p>
-            <p>초가지붕 둥근 박 꿈 꿀때</p>
-            <Fruits />
-            <p>노랗게 익어만 가는</p>
-         </section>
-      )
-   }
-}
+// 상위 컴포넌트
+const Yellow = () => {
+  return (
+    <section>
+      <p>허수아비 팔 벌려 웃음짓고</p>
+      <p>초가지붕 둥근 박 꿈 꿀때</p>
+      <Fruits />
+      <p>노랗게 익어만 가는</p>
+    </section>
+  );
+};
 
-const myApp = ReactDOM.createRoot( document.getElementById( 'root' ) );
-myApp.render( <Yellow /> );
+// 렌더링
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( <Yellow /> );
