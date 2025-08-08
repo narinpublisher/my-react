80506d0e7c6904409e72a960cef2fea5c6aa5dbe
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2025-08-08 12:13:44 +0900

    Add files via upload

---
+++ react-11.10.1.js
@@ -0,0 +1,17 @@
+const { useState } = React;
+
+const Wind = () => {
+  const [sunset] = useState('노을');
+
+  return (
+    <p>
+      바람이 머물다간 들판에<br />
+      모락모락 피어나는 저녁연기<br />
+      색동옷 갈아입은 가을 언덕에<br />
+      빨갛게 <b className="red">{sunset}</b>이 타고 있어요.
+    </p>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Wind />);
