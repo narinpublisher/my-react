2a3f42ed07c19d8f7c081c9ff8d79172b3974fe5
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2025-08-08 19:44:48 +0900

    Add files via upload

---
+++ react-11.12.1.js
@@ -0,0 +1,11 @@
+const Autumn = () => {
+  return (
+    <p>
+      가을바람 머물다간 들판에<br />
+      모락모락 피어나는 저녁연기
+    </p>
+  );
+};
+
+const yellow = ReactDOM.createRoot(document.getElementById('root'));
+yellow.render(<Autumn />);
