f8a6b1a7a06ed09fa0c96df72c3adb423ad6f4fa
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-02-01 17:02:08 +0900

    Add files via upload

---
+++ 5.1.4.1.e.js
@@ -0,0 +1,5 @@
+const myApp = <h1>React Test</h1>;
+
+// Location where the content will be rendered
+const root = ReactDOM.createRoot( document.getElementById( 'root' ) ); 
+root.render( myApp ); // Content to render
