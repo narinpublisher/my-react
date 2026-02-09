4c60c2c2f1e677d9bdec79ba141516845044b499
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-02-09 20:10:06 +0900

    Add files via upload

--- BarleyTree-1.js
+++ BarleyTree-1.js
@@ -1,28 +1,29 @@
 const Barley= ()=>  {
-  const [mun , setMun] = React.useState('후문 뒤  ');
-  const [communism] = React.useState('우물 곁에 ');
-  const [eradicate] = React.useState('서 있는 보리수');
+  const [mun , setMun] = React.useState('by the stream ');
+  const [communism] = React.useState('There stands a linden tree ');
+  const [eradicate] = React.useState('I dreamed in its shadow');
 
   const change = () => {
-    setMun('성문 앞  ');
+    setMun('By the fountain ');
   };
 
   return (
     <section>
-      <h1 className="orange">빌헬름 뮐러_ 보리수</h1>
+      <h1 className="orange">Wilhelm Muller - The Linden Tree</h1>
       <p>
-        <b className="aqua">{mun}</b>
-        {communism}
-        {eradicate}
+        By the fountain<br />
+        <b className="aqua">{mun}</b><br />
+        {communism}<br />
+        {eradicate}<br />
         <br />
-        나는 그 그늘 아래 단 꿈을 보았네
-        <br />
-        가지에 희망의 말 새기어 놓고서
-        <br />
-        기쁘나 슬플 때나 찾아온 나무 밑
+        So many a sweet dream<br />
+        I carved in its bark<br />
+        So many a cherished word<br />
+        It drew me, in joy and sorrow<br />
+        Ever nearer to it
       </p>
       <button type="button" onClick={change}>
-        텍스트 변경하기
+        Change the Text
       </button>
     </section>
   );
