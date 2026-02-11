95c418886a7d749a8173d6b81cd53cc5404fca55
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-02-11 19:22:46 +0900

    Add files via upload

---
+++ react-11.12.e.js
@@ -0,0 +1,11 @@
+const Autumn = () => {
+  return (
+    <p>
+      In the field where autumn winds once lingered<br />
+      Evening smoke rises gently into the sky
+    </p>
+  );
+};
+
+const yellow = ReactDOM.createRoot(document.getElementById('root'));
+yellow.render(<Autumn />);
