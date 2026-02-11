afb0e41b4c7fe4868e7f5e8bdf5ca93d7729ff26
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-02-11 15:34:38 +0900

    Add files via upload

--- scarecrow-1.js
+++ scarecrow-1.js
@@ -1,24 +1,19 @@
     const { useState, useEffect } = React;
 
-      function Sunset(props) {
-        const [fruit, setFruit] = useState(' 가을의 달빛 ');
-
-        useEffect(() => {
-          setFruit(props.basket);
-        }, [props.basket]);
-
-        return React.createElement(
-          'p',
-          null,
-          '허수아비 팔 벌려 웃음짓고',
-          React.createElement('br'),
-          '초가지붕 둥근 박 꿈 꿀때',
-          React.createElement('br'),
-          '고개숙인 ',
-          React.createElement('b', { className: 'orange' }, fruit),
-          '노랗게 익어만 가는'
-        );
-      }
-
-      const root = ReactDOM.createRoot(document.getElementById('root'));
-      root.render(React.createElement(Sunset, { basket: ' 논밭의 열매 ' }));
+    function Sunset({ basket }) {
+      const [fruit, setFruit] = useState(' Autumn Moonlight ');
+      useEffect(() => {
+        setFruit(basket);
+      }, [basket]);
+      return (
+        <p>
+          The scarecrow spreads its arms and smiles<br />
+          As the round gourd dreams on the thatched roof<br />
+          Bowing quietly,
+          <b className='orange'>{fruit}</b>
+          ripens into golden yellow
+        </p>
+      );
+    }
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<Sunset basket=" Harvest from the Fields " />);
