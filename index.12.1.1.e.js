40c0dd7a1fa43c21daee3f5959995d4b23f00e19
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-02-14 18:45:12 +0900

    Update index.12.1.1.e.js

--- index.12.1.1.e.js
+++ index.12.1.1.e.js
@@ -1,1 +1,19 @@
-function MonumentTree ( props ) {	return (		<>			<i className='orange'> { props.sky }</i><br />			Longing gathers like moss on every branch		</>	)} function Rainstorm () {	return (		<> 			Through long years of wind and rain,<br />			An unnamed monument stands,<br />			<MonumentTree  sky=“The distant sky of childhood friends left behind />		</>	)}const democracy = ReactDOM.createRoot ( document.getElementById('root') );democracy.render (  <Rainstorm />  );
+function MonumentTree ( props ) {
+	return (
+		<>
+			<i className='orange'> { props.sky }</i><br />
+			Longing gathers like moss on every branch
+		</>
+	)
+} 
+function Rainstorm () {
+	return (
+		<> 
+			Through long years of wind and rain,<br />
+			An unnamed monument stands,<br />
+			<MonumentTree  sky=“The distant sky of childhood friends left behind" />
+		</>
+	)
+}
+const democracy = ReactDOM.createRoot ( document.getElementById('root') );
+democracy.render (  <Rainstorm />  );
