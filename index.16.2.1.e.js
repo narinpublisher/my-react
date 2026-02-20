55a95579eb8c6d203db5ac60b718997d0335aa6b
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-02-20 12:55:39 +0900

    Add files via upload

---
+++ index.16.2.1.e.js
@@ -0,0 +1,28 @@
+const Sunset = () => {
+
+  const [name, setName] = useState('');
+
+  return (
+    <section>
+      <strong>
+	On the field where the wind once lingered,<br />
+	Evening smoke softly rises.
+      </strong>
+
+      <form>
+        <br />
+        <label>
+          What is the title of this song? &nbsp;
+          <input
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </label>
+      </form>
+    </section>
+  );
+};
+
+const textBox = ReactDOM.createRoot(document.getElementById('root'));
+textBox.render(<Sunset />);
