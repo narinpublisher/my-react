12771bba7aeb2a6bdd4bcf3bba2f39390c6a0cf3
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-02-20 19:31:08 +0900

    Add files via upload

--- index.16.2.3.e.js
+++ index.16.2.3.e.js
@@ -13,10 +13,10 @@ const Tree = ()=> {
  		<section>
 			<h1>Art Song – "Memorial Marker"</h1>
 			<form onSubmit= { submitGo }>
-			   	<input type= 'text' name= 'txt1'  value= { inputs.txt1 || ' '}
+			   	<input type= 'text' name= 'txt1'  value= { inputs.txt1 || ''}
 				onChange= { submitGoGo } />
 				<br />Deep in the valley, on a sunny hillside<br />
-				<input type= 'text' name= 'txt2'  value= { inputs.txt2 || ' ' }
+				<input type= 'text' name= 'txt2'  value= { inputs.txt2 || '' }
 				onChange= { submitGoGo } />
 				<br />For the nameless wooden cross, left behind.<br />
 				<button>Submit</button>
@@ -26,5 +26,4 @@ const Tree = ()=> {
 }
 
 const textBox = ReactDOM.createRoot ( document.getElementById ('root') );
-
 textBox.render ( <Tree /> );
