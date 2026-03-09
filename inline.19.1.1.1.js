cae9203583d6a144496df2a87b724e789545cc91
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-03-09 14:06:07 +0900

    Add files via upload

--- inline.19.1.1.1.js
+++ inline.19.1.1.1.js
@@ -1,13 +1,13 @@
 const MyApp = () => {
-	return (
-		<>
-			<h1 style= {{color: 'red' , textShadow: '-2px -2px 7px orange'}}>
-			   인라인 스타일
-			</h1>
-			<p>해당 스타일 디자인이 지정 요소에 즉각적으로 적용됩니다.</p>
-		</>
-	);	
+  return (
+    <>
+      <h1 style={{ color: 'red', textShadow: '-2px -2px 7px orange' }}>
+        Inline Style
+      </h1>
+      <p>The style design is immediately applied to the selected element.</p>
+    </>
+  );
 }
 
-const addStyle = ReactDOM.createRoot ( document.getElementById ( 'root' ) );
-addStyle.render ( <MyApp /> );
+const addStyle = ReactDOM.createRoot(document.getElementById('root'));
+addStyle.render(<MyApp />);
