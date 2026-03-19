8ee6e5ce2007a9b4ba322e0ad99a389cc202c880
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-03-19 16:54:04 +0900

    Add files via upload

---
+++ 21.3.1.e.js
@@ -0,0 +1,21 @@
+function Timer() {
+  const [ count , setCount ] = React.useState(0);
+
+  React.useEffect(  () => {
+    const timer = setTimeout(() => {
+      	setCount(  ( prevCount  ) => prevCount + 1 );
+    }, 1000 );
+
+    return () => clearTimeout(  timer  );
+  } ,  [  count  ] ); 
+
+  return (
+    <h3 style=  {{ textAlign : “center” }}>
+      Time elapsed since this page was rendered:
+      <b style=  {{ color : “red” , fontSize : “1.5rem” }}> {  count  } </b> seconds
+    </h3>
+  );
+}
+
+const root = ReactDOM.createRoot( document.getElementById( “root” ) ) ;
+root.render(  <Timer />  );
