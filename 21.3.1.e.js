0c1e6aca8d59e96afdd10a136548dedd78e0fa47
narinpublisher <150247503+narinpublisher@users.noreply.github.com>
2026-03-19 17:02:02 +0900

    Add files via upload

--- 21.3.1.e.js
+++ 21.3.1.e.js
@@ -10,12 +10,12 @@ function Timer() {
   } ,  [  count  ] ); 
 
   return (
-    <h3 style=  {{ textAlign : “center” }}>
+    <h3 style=  {{ textAlign : "center" }}>
       Time elapsed since this page was rendered:
-      <b style=  {{ color : “red” , fontSize : “1.5rem” }}> {  count  } </b> seconds
+      <b style=  {{ color : "red" , fontSize : "1.5rem" }}> {  count  } </b> seconds
     </h3>
   );
 }
 
-const root = ReactDOM.createRoot( document.getElementById( “root” ) ) ;
+const root = ReactDOM.createRoot( document.getElementById( "root" ) ) ;
 root.render(  <Timer />  );
